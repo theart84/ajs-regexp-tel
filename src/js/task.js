4c4da17545a03de2f1b4e7850af4935758e76354
theart84 <artshadow74@gmail.com>
2021-02-19 22:40:11 +0500

    minor fixes

--- src/js/task.js
+++ src/js/task.js
@@ -1,11 +1,7 @@
 export default function validatePhone(phone) {
   const normalizePhone = phone.replace(/^\+|[-()\s]*/g, '');
   if (normalizePhone.length === 11) {
-    return `+${normalizePhone.replace(/^\d/, '7')}`;
+    return `${normalizePhone.replace(/^\d/, '+7')}`;
   }
-  if (normalizePhone.length > 11) {
-    return `+${normalizePhone}`;
-  }
-  normalizePhone.replace(/^\d/, '7');
-  return null;
+  return `+${normalizePhone}`;
 }
